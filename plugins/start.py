ccec3197b45bbc35c715ae0745f88c284b998874
LAKHAN29 <112367600+LAKHAN29@users.noreply.github.com>
2023-06-11 22:44:49 +0530

    Add files via upload

---
+++ plugins/start.py
@@ -0,0 +1,23 @@
+from .. import bot
+from telethon import events
+import asyncio
+
+@bot.on(events.NewMessage(incoming = True, pattern ="/start"))
+async def start(event):
+  await event.reply("Hello This is Bot")
+  
+@bot.on(events.NewMessage(incoming = True, pattern ="/get"))
+async def start(event):
+  a=await event.reply("Hello This is get command ")
+  await asyncio.sleep(3)
+  await a.edit("this is edited message")
+  
+@bot.on(events.NewMessage(incoming = True, pattern ="/eval"))
+async def start(event):
+  await event.reply("Hello This is eval command ")
+  
+  
+  
+  
+  
+  
